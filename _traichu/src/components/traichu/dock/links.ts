d0ba1b9fa78244afa37388c27f572db310b88a7c
VKTRenokh <viktorenokh@gmail.com>
2024-01-22 10:53:34 +0200

    feat(binds): add chess.com rederict bind

--- _traichu/src/components/traichu/dock/links.ts
+++ _traichu/src/components/traichu/dock/links.ts
@@ -3,7 +3,7 @@ import calendarIcon from './svg/calendar.svg';
 import notionIcon from './svg/notion.svg';
 import githubIcon from './svg/github.svg';
 import keepIcon from './svg/keep.svg';
-import type {Link} from './types/link';
+import type { Link } from './types/link';
 
 const github = 'https://github.com';
 const gitlab = 'https://gitlab.com';
@@ -15,12 +15,13 @@ const discord = 'https://discord.com/app';
 const tuta = 'https://tuta.io';
 
 export const links: Link[] = [
-  {href: googleMail, icon: mailIcon, bind: 'm'},
-  {href: calendar, icon: calendarIcon, bind: 'c'},
-  {href: googleKeep, icon: keepIcon, bind: 'k'},
-  {href: notion, icon: notionIcon, bind: 'n'},
-  {href: github, icon: githubIcon, bind: 'g'},
-  {href: gitlab, bind: 'G'},
-  {href: discord, bind: 'D'},
-  {href: tuta, bind: 'M'},
+  { href: googleMail, icon: mailIcon, bind: 'm' },
+  { href: calendar, icon: calendarIcon, bind: 'c' },
+  { href: googleKeep, icon: keepIcon, bind: 'k' },
+  { href: notion, icon: notionIcon, bind: 'n' },
+  { href: github, icon: githubIcon, bind: 'g' },
+  { href: gitlab, bind: 'G' },
+  { href: discord, bind: 'D' },
+  { href: tuta, bind: 'M' },
+  { href: 'https://chess.com', bind: 'C' },
 ];
