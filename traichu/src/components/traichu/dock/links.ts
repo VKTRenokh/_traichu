9a84c8ea453d3d71744b12647e65b5a2616b7012
VKTRenokh <viktorenokh@gmail.com>
2025-04-03 11:36:20 +0300

    refactor: remove useless constants

--- traichu/src/components/traichu/dock/links.ts
+++ traichu/src/components/traichu/dock/links.ts
@@ -1,27 +1,18 @@
-import mailIcon from './svg/mail.svg';
-import calendarIcon from './svg/calendar.svg';
-import notionIcon from './svg/notion.svg';
-import githubIcon from './svg/github.svg';
-import keepIcon from './svg/keep.svg';
-import type { Link } from './types/link';
-
-const github = 'https://github.com';
-const gitlab = 'https://gitlab.com';
-const notion = 'https://notion.so';
-const googleKeep = 'https://keep.google.com';
-const calendar = 'https://calendar.google.com';
-const googleMail = 'https://mail.google.com';
-const discord = 'https://discord.com/app';
-const tuta = 'https://tuta.io';
+import mailIcon from './svg/mail.svg'
+import calendarIcon from './svg/calendar.svg'
+import notionIcon from './svg/notion.svg'
+import githubIcon from './svg/github.svg'
+import keepIcon from './svg/keep.svg'
+import type { Link } from './types/link'
 
 export const links: Link[] = [
-  { href: googleMail, icon: mailIcon, bind: 'm' },
-  { href: calendar, icon: calendarIcon, bind: 'c' },
-  { href: googleKeep, icon: keepIcon, bind: 'k' },
-  { href: notion, icon: notionIcon, bind: 'n' },
-  { href: github, icon: githubIcon, bind: 'g' },
-  { href: gitlab, bind: 'G' },
-  { href: discord, bind: 'D' },
-  { href: tuta, bind: 'M' },
+  { href: 'https://mail.google.com', icon: mailIcon, bind: 'm' },
+  { href: 'https://calendar.google.com', icon: calendarIcon, bind: 'c' },
+  { href: 'https://keep.google.com', icon: keepIcon, bind: 'k' },
+  { href: 'https://notion.so', icon: notionIcon, bind: 'n' },
+  { href: 'https://github.com', icon: githubIcon, bind: 'g' },
+  { href: 'https://gitlab.com', bind: 'G' },
+  { href: 'https://discord.com/app', bind: 'D' },
+  { href: 'https://tuta.io', bind: 'M' },
   { href: 'https://chess.com', bind: 'C' },
-];
+]
